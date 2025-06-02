428d6b7cee1883322ced175da42376e33d122746
Vaibhav Singh <singhvaibhav654@gmail.com>
2025-06-02 15:31:12 +0530

    herosection

--- src/app/component/fund_section/heromission.js
+++ src/app/component/fund_section/heromission.js
@@ -8,7 +8,7 @@ export default function Heromission() {
     <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
       <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Purpose of our financial campaign
       </h1>
-      <p class="mb-8 leading-relaxed">I came across soldiers who were facing multiple health problems. There were certain soldiers receiving little to no pensions, making it difficult to support their families. This initiative is a seed planted to support the Indian Army, the Veterans, and their families.</p>
+      <p class="mb-8 leading-relaxed">There are soldiers who are facing serious health challenges and financial hardships. Some veterans receive little to no pension, making it difficult to support their families and live with dignity. This initiative is a small but meaningful step—planted like a seed—with the hope of growing into lasting support for the Indian Army, its veterans, and their loved ones.</p>
       <div class="flex justify-center">
      </div>
     </div>
